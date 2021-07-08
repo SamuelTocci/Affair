d99eb78164422334f1a2993576acbe5d78ffbf86
Samuel Tocci <samuel.tocci@gmail.com>
2021-07-08 14:48:49 +0200

    Big text component + intro

---
+++ src/components/BigText.js
@@ -0,0 +1,16 @@
+import react from "react"
+import TextPictureStyle from './BigText.css'
+
+const BigText = () => {
+    return (
+        <div className={"intro"}>
+            <p>
+                Kwalitatieve <br/>
+                elektronische muziekevenementen <br/>
+                in een prikkelende omgeving.
+            </p>
+        </div>
+    );
+}
+
+export default BigText;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,13 @@
 import react from 'react'
 import {Route, Switch} from "react-router-dom";
 import About from "./About";
+import BigText from "../components/BigText";
 
 const Home = () => {
     return (
-        <div>
-        </div>
+        <body>
+            <BigText/>
+        </body>
     );
 }
 
