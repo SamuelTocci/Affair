33098c0e8067a159a99faf9d6be0035a3f6447d1
Samuel Tocci <samuel.tocci@gmail.com>
2021-07-09 16:29:19 +0200

    picture text component fixed picture alignement

--- src/components/PictureText.js
+++ src/components/PictureText.js
@@ -3,20 +3,20 @@ import PictureTextStyle from "./PictureText.css"
 
 const PictureText = (props) => {
     return (
-        <>
-        <div className={"Category"}>
-            {props.category}
+        <div className={"container"}>
+            <div className={"Category"}>
+                {props.category}
+            </div>
+            <div>
+            <div className={"Title"}>
+                <p>{props.title}</p>
+            </div>
+            <div className={"Text"}>
+                <p>{props.text}</p>
+            </div>
+            </div>
+            <div className={"Img"}><img src={props.img} width={600}/></div>
         </div>
-        <div className={"Title"}>
-            <p className={"Img"}><img src={props.img} width={600}/></p>
-            <p>{props.title}</p>
-        </div>
-        <div className={"Text"}>
-            <p>
-                {props.text}
-            </p>
-        </div>
-        </>
     );
 }
 
